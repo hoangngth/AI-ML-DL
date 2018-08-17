27a5f37dc214288c0f55112845a53d5a9090c9ef
hoangngth <42316439+hoangngth@users.noreply.github.com>
2018-08-17 19:23:00 +0700

    Add files via upload

--- Sentiment Analysis/Word2Vec/train_word2vec_model.py
+++ Sentiment Analysis/Word2Vec/train_word2vec_model.py
@@ -1,28 +1,28 @@
-# Vietnam Word2Vec Model 
+# Word2Vec Model 
 
 import os
-#import sys
-import codecs
 import gensim
 import time
 
 start_time = time.time()
 
 class MySentences(object):
-     def __init__(self, dirname):
-         self.dirname = dirname
+    def __init__(self, dirname):
+        self.dirname = dirname
 
-     def __iter__(self):
-         for fname in os.listdir(self.dirname):
-             for line in codecs.open(os.path.join(self.dirname, fname), 'r', 'utf-8'):
-                 yield line.split()
+    
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            file = open(os.path.join(self.dirname, fname), 'r', encoding='utf8')
+            for line in file:
+                yield line.split()
 
 dirData='C:/Users/Vaio/Desktop/Sentiment Analysis/Word2Vec/Tokenizer/VNTQcorpus_small_tok'
 pathModelBin='C:/Users/Vaio/Desktop/Sentiment Analysis/Word2Vec/trained_word2vec/VNTQw2v.bin'
 pathModelTxt='C:/Users/Vaio/Desktop/Sentiment Analysis/Word2Vec/trained_word2vec/VNTQw2v.txt'
 
 if __name__ == '__main__':
-    sentences = MySentences(dirData) # a memory-friendly iterator
+    sentences = MySentences(dirData)
     
     print("Training Word2Vec model...")
     model = gensim.models.Word2Vec(sentences, size=300, window=10, min_count=10, sample=0.0001, workers=4, sg=0, negative=10, cbow_mean=1, iter=5)
