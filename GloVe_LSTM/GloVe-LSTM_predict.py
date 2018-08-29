902356b640773ce521d8ffa453f7b367b9163b7f
hoangngth <42316439+hoangngth@users.noreply.github.com>
2018-08-29 15:00:31 +0700

    Update GloVe-LSTM_predict.py

--- GloVe_LSTM/GloVe-LSTM_predict.py
+++ GloVe_LSTM/GloVe-LSTM_predict.py
@@ -4,26 +4,21 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 
-model = load_model(getcwd()+'GloVe-LSTM_model.h5')
+model = load_model(getcwd()+'/GloVe-LSTM_model.h5')
 
-dataset = ['Well done!',
-		'Good work',
-		'Great effort',
-		'nice work',
-		'Excellent!',
-		'Weak',
-		'Poor effort!',
-		'not good',
-		'poor work',
-		'Could have done better.']
+with open(getcwd()+'/dataset/amazon_sentiment/sentence.txt', encoding='utf-8', errors='ignore') as f:
+    dataset = f.readlines()
+dataset = [x.strip('\t\n') for x in dataset]
 
 t = Tokenizer()
 t.fit_on_texts(dataset)
 
-max_len = 4
+max_len = 20
 user_sentence = input('Enter your sentence: ')
 temp_sentence = []
 for word in user_sentence.split(' '):
+    if word not in t.word_index:
+        continue
     temp_sentence.append(t.word_index[word])
     temp_sentence_padded = pad_sequences([temp_sentence], maxlen=max_len) 
 print('Sentiment: ',(model.predict(np.array([temp_sentence_padded][0]))[0][0]))
